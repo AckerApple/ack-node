6f326997e49b3ad0edbac88a69444af3fe7375b6
ackerapple <acker.dawn.apple@gmail.com>
2017-02-01 11:20:51 -0500

    console debug change order of whats output

--- modules/router.js
+++ modules/router.js
@@ -337,7 +337,7 @@ function getMorganDefaultFormat(options,add){
 	options = options || {}
 	const status = options.stream ? ':status' : ':colored-status'
 	const method = options.stream ? ':method' : ':colored-method'
-	let rtn = status+' :url-short '+method+' :res[content-length] :response-time ms :remote-addr :remote-user :device-name :browser-name'
+	let rtn = status+' '+method+' :url-short :res[content-length] :response-time ms :remote-addr :remote-user :device-name :browser-name'
 	if(add){
 		rtn += ' '+add
 	}
